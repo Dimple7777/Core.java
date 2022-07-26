2fc17884878d1d5da557b496afdcda0d7e0e32e4
Dimple7777 <94853760+Dimple7777@users.noreply.github.com>
2022-07-26 13:27:32 +0530

    TEST

---
+++ CalciWorking.java
@@ -0,0 +1,38 @@
+class CalciWorking{
+ public static void main(String args[]){
+     Calci.addy();
+	 Calci.subby();
+	 
+	 Calci obj = new Calci();
+	 obj.diviji();
+	 obj.mull();
+   
+ 
+ 
+   }
+ }
+ 
+   class Calci{
+     static void addy(){
+        System.out.println(" hi i am addy ");
+  
+      }
+     static void subby(){
+ 
+        System.out.println(" hi i am subby ");
+ 
+      }
+     void diviji(){
+ 
+     System.out.println(" hi i am diviji ");
+      }
+ 
+     void mull(){
+ 
+       System.out.println(" null.. ");
+ 
+     }
+ 
+
+
+}
---
+++ ThissTest.java
@@ -0,0 +1,43 @@
+class ThissTest{
+  static int a= 20;
+ public static void main(String args[]){
+   System.out.println(" My age is = " + a);
+
+   System.out.println(" My name is Gurleen kaur ");   
+   ThissTest obj = new ThissTest();
+   obj.method1();
+   System.out.println(" My hobbies are dancing and sketching ");
+ }
+  
+   
+
+
+  void method1(){
+ 
+
+
+  }
+}
+
+
+// Ques: 3 
+// 1) .java file is used to compile the program or source code and .class is used to run the program .
+// 2) Java is not a complete OOPs language as their are use of static method and datatypes which are not objects whereas if we talk about OOPs language
+//  it deals with objects only.
+// 3) NR-WA : it is a type of function in which we have no return value and we pass the arguments.
+//   Example: 
+                //void main(int a)
+	  //here void indicates no return value and int a symbolizes the arguments.
+
+// Ques: 5
+// Wrapper class : when we convert the datatype into the object that is known as wrapper class
+//   Example: 
+                // Interger i2= int i;
+
+
+//Ques:  2 
+//  class name : 	1) class Class2 	2)class Test
+//  function name  :  1) void main()    2) int test()
+//  variable  :        1)  int a=7;           2)double b= 12.555;
+//  static variable  :  1) static int b= 7;   2)static double = 12.5555;
+                      
